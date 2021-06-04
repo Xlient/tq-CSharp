7d338605a93a1e70aacdc03e8dcbe9b782112fac
Xlient <lakaleigh.harris@gmail.com>
2021-06-03 21:25:05 -0400

    cleaned up code

--- levels/vr_mission_template/objectives/variables_and_dataTypes/validator.js
+++ levels/vr_mission_template/objectives/variables_and_dataTypes/validator.js
@@ -1,55 +1,21 @@
-const { exec} = require('child_process');
-const {spawn} =  require('child_process');
-const path = require('path');
-const { cwd, stderr, stdin } = require('process');
 
+const { ExecuteCode } = require('../../validation');
 module.exports = async function (helper) {
 
-  try{
-      const codePath = helper.env.TQ_CSHARP_WORKSPACE_CODE;
-      const pathToValidator = path.join(__dirname +"\\validate");
-      let bufferdStdout = "";
-      let bufferdStderr = "";
+  try {
+    const codePath = helper.env.TQ_CSHARP_WORKSPACE_CODE;
+     const results = await ExecuteCode(codePath);
 
-      const dotnetRun = spawn('dotnet',['run',`${codePath}`],
-      { cwd: pathToValidator , shell: true});
-
-
-        dotnetRun.on('error', (err) => {
-          console.error(`${err}`);
-        });
-
-        dotnetRun.stderr.on('data', (data) => {
-              bufferdStderr += `${data}`;
-          });
-
-        dotnetRun.stdout.on('data', (data) => {
-            
-              bufferdStdout =`${data}`;
-              console.log(bufferdStdout);
-              
-            });
-
-            dotnetRun.on('close', (code) => {
-              
-                console.log(`${code}`);
-              
-            });
-          
-
-          if(bufferdStdout.includes("OFF") == true && bufferdStdout.includes('PASS') == true){
-            console.log(bufferdStdout.includes("OFF"));
-            helper.fail(`Oops something went wrong with your code. Check the example code and try again`);
-            
-          }
-          else
-            { 
-              helper.success(`Great job!`);
-            }
-        
-        }    
-  catch(e) 
-  {
-    helper.fail(e.message);
+    if(results.stdout.search("OFF") == -1 && results.stdout.search("PASS") == -1){
+      helper.fail(`Oops something went wrong with your code. Check the example code and try again`);
+    }
+    else
+      { 
+        helper.success(`Great job!`);
+      }
+    }
+    catch(e)
+    {
+      helper.fail(e.message);
     }
 };
