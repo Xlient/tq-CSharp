26b12a390ec831beeb1e2c5cffb7bd72cb029b80
La'Kaleigh Harris <35268101+Xlient@users.noreply.github.com>
2023-08-10 17:17:05 +0000

    use correct env name

--- levels/botnet/objectives/3_first_Csharp_program/validator.js
+++ levels/botnet/objectives/3_first_Csharp_program/validator.js
@@ -1,4 +1,4 @@
-//@foobar54350qahj89jhfd685875asaf43
+//@foobar54350qahj89jhfd685875asaf43as
 const  { anaylzeAndExcuteCode } = require('../../validation');
 module.exports = async function (helper) {
 
